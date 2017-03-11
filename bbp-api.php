ca1a0e02ffd63daae37eae60b904cb0514ae1e6d
ePascalC <pascal.casier@gmail.com>
2017-03-11 14:51:34 +0100

    Copy of v1.0.2 from w.org

--- bbp-api.php
+++ bbp-api.php
@@ -1,19 +1,67 @@
 <?php
 /*
-Plugin Name: bbPress API
-Description: Create an API for bbPress forums, topics & replies.
+Plugin Name: bbP API
+Description: A bbp REST API.
+Plugin URI: https://wordpress.org/plugins/bbp-api/
 Author: Pascal Casier
-Author URI: http://casier.eu/wp-dev
-Version: 0.1
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Author URI: http://casier.eu/wp-dev/
+Text Domain: bbp-api
+Version: 1.0.2
+License: GPL2
 */
+
+// No direct access
+if ( !defined( 'ABSPATH' ) ) exit;
+
+define ('BBPAPI_VERSION' , '1.0.2');
+
+if(!defined('BBPAPI_PLUGIN_DIR'))
+	define('BBPAPI_PLUGIN_DIR', dirname(__FILE__));
+if(!defined('BBPAPI_URL_PATH'))
+	define('BBPAPI_URL_PATH', plugin_dir_url(__FILE__));
+
+
+include(BBPAPI_PLUGIN_DIR . '/inc/forums.php');
+include(BBPAPI_PLUGIN_DIR . '/inc/topics.php');
+include(BBPAPI_PLUGIN_DIR . '/inc/replies.php');
+include(BBPAPI_PLUGIN_DIR . '/inc/topic-tags.php');
+include(BBPAPI_PLUGIN_DIR . '/inc/stats.php');
+
+/*
+ * Register all routes
+*/
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'bbp-api/v1', '/forums/', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_forums',
+	) );
+	register_rest_route( 'bbp-api/v1', '/forums/(?P<id>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_forums_one',
+	) );
+	register_rest_route( 'bbp-api/v1', '/topics/', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_topics',
+	) );
+	register_rest_route( 'bbp-api/v1', '/topics/(?P<id>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_topics_one',
+	) );
+	register_rest_route( 'bbp-api/v1', '/replies/', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_replies',
+	) );
+	register_rest_route( 'bbp-api/v1', '/replies/(?P<id>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_replies_one',
+	) );
+	register_rest_route( 'bbp-api/v1', '/topic-tags/', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_topic_tags',
+	) );
+	register_rest_route( 'bbp-api/v1', '/stats/', array(
+		'methods' => 'GET',
+		'callback' => 'bbp_api_stats',
+	) );
+} );
