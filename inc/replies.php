da04ec091550b0a6424ab124f51f3875f5bc491c
Daniel T <me@danielturton.net>
2017-05-03 16:18:03 -0500

    removed admin email from code.

--- inc/replies.php
+++ inc/replies.php
@@ -38,20 +38,20 @@ function bbp_api_replies() {
 */
 function bbp_api_replies_one($data) {
 	$all_reply_data = bbp_api_replies_info($data['id']);
-	$all_reply_data['user'] = get_user_by("email", "admin@local.dev");
 	return $all_reply_data;
 }
 
 /*
  * Setting up POST for new replies via API.
  * Example code in BBPress here: includes/core/update.php
+ * requires content and email fields in the POST data to work.
  * array data: submitted information from POST requested
  * return string reply_id: id number for accepted post
 */
 function bbp_api_replies_post($data) {
 	$all_reply_data = bbp_api_replies_info($data['id']);
 	$all_reply_data['content'] = $data['content'];
-	$myuser = get_user_by("email", "admin@local.dev");
+	$myuser = get_user_by("email", $data['email']);
 	$reply_id = bbp_insert_reply(
     array(
       'post_parent'  => $all_reply_data['topic_id'],
